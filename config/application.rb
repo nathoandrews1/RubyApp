9cd49e8173e30a6658939559ae98e7b050d566e4
nathoandrews1 <nathoandrews@gmail.com>
2023-01-14 09:45:10 +0000

    Fixed bug with rails server launch.

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,6 @@ module CarHireApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    config.active_record.observers = :contact_observer
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
